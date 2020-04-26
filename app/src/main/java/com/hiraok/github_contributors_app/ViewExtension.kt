dd5b5f039cdb25a7a0473e925e0006e9270ac5ee
hiraok <khirao.dev.work@gmail.com>
2020-04-27 00:01:39 +0900

    ViewExtensin追加

--- app/src/main/java/com/hiraok/github_contributors_app/ViewExtension.kt
+++ app/src/main/java/com/hiraok/github_contributors_app/ViewExtension.kt
@@ -3,11 +3,12 @@ package com.hiraok.github_contributors_app
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
 
 object ViewExtension {
     @JvmStatic
     @BindingAdapter("imageUrl")
     fun ImageView.loadImage(url: String?) {
-        Picasso.get().load(url).into(this)
+        Picasso.get().load(url).transform(CircleTransform()).into(this)
     }
 }
